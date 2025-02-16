c7942caa6c39414197441fb7f3dcad4cee650fd3
tahsina836 <tahsinaafrin836@gmail.com>
2025-02-16 10:47:46 +0600

    commit 5

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Header/Header'
 import ExploreMenu from '../../ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../FoodDisplay/FoodDisplay'
 
 
 
 const Home = () => {
+
+
+    const [category, setCategory] = useState("All");
+
+
     return (
         <div>
             <Header />
-            <ExploreMenu/>
-     
+            <ExploreMenu category={category} setCategory={setCategory} />
+            <FoodDisplay category={category} />
         </div>
     )
-}
 
+}
 export default Home
