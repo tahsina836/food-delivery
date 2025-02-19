d2a626e4819e634f2d3dc99c67d1df3d2949d79c
tahsina836 <tahsinaafrin836@gmail.com>
2025-02-20 00:01:38 +0600

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home/Home'
 import Cart from './components/pages/Cart/Cart'
 import PlaceOrder from './components/pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
   return (
     <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' elements={<Home />} />
           <Route path='/cart' elements={<Cart />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home");
 
@@ -34,7 +34,7 @@ const Navbar = () => {
 
 
                 </div>
-                <button>Sign in</button>
+                <button onClick={() => setShowLogin(true)}>Sign in</button>
             </div>
 
 
