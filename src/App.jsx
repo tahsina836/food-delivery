d93f9462119444a81802417cbce7a5f9b9063e46
tahsina836 <tahsinaafrin836@gmail.com>
2025-02-19 21:22:49 +0600

    commit 9

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,27 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home/Home'
 import Cart from './components/pages/Cart/Cart'
 import PlaceOrder from './components/pages/PlaceOrder/PlaceOrder'
-
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' elements={<Home />} />
+          <Route path='/cart' elements={<Cart />} />
+          <Route path='/order' elements={<PlaceOrder />} />
+        </Routes>
+        <Home />
 
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' elements={<Home />} />
-        <Route path='/cart' elements={<Cart />} />
-        <Route path='/order' elements={<PlaceOrder />} />
-      </Routes>
-      <Home />
+      </div>
 
-    </div>
+      <Footer />
+
+    </>
+
+
   )
 }
 
